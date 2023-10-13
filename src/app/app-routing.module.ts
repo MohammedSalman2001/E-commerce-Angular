19ccf035a80ee2cb4eab0ad7adefc6f56439c2e8
MohammedSalman2001 <salmannisthar123@gmail.com>
2023-10-13 07:41:12 +0530

    register-part-implement-ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'/process/landing',pathMatch:'full'},
+  { path: 'process', loadChildren: () => import('./modules/process/process.module').then(m => m.ProcessModule) },
+  { path: 'security', loadChildren: () => import('./modules/security/security.module').then(m => m.SecurityModule) },
+  { path: 'console', loadChildren: () => import('./modules/console/console.module').then(m => m.ConsoleModule) },
+  { path: 'share', loadChildren: () => import('./modules/share/share.module').then(m => m.ShareModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
