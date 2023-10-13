759a020d8fc8352374989622adc92c08b3f30812
MohammedSalman2001 <salmannisthar123@gmail.com>
2023-10-13 08:49:16 +0530

    interceptor add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {HttpManagerInterceptor} from "./modules/share/services/interceptor/http-manager.interceptor";
 
 @NgModule({
   declarations: [
@@ -12,9 +14,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [{provide:HTTP_INTERCEPTORS,useClass:HttpManagerInterceptor,multi:true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
